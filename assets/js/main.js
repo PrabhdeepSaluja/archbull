303c9f1a513e14e05a40a0abbf841c4fbec166eb
unknown <prabhdeep.saluja123@gmail.com>
2024-05-29 12:31:22 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,6 @@ window.addEventListener('scroll', scrollActive)
 function scrollHeader(){
     const header = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 400) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 500) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
